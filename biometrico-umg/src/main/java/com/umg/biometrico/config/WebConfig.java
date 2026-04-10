37cc26bd7e42772cf329e18ee6c85db0e5576c23
Dfrancoc256 <dfrancoc5@miumg.edu.gt>
2026-04-09 21:57:57 -0600

    Primer commit sistema biometrico

---
+++ biometrico-umg/src/main/java/com/umg/biometrico/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.umg.biometrico.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        Path rutaUploads = Paths.get("uploads").toAbsolutePath().normalize();
+        Path rutaFotosPersonas = Paths.get("fotos_personas").toAbsolutePath().normalize();
+
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + rutaUploads.toString() + "/");
+
+        registry.addResourceHandler("/fotos_personas/**")
+                .addResourceLocations("file:" + rutaFotosPersonas.toString() + "/");
+    }
+}
