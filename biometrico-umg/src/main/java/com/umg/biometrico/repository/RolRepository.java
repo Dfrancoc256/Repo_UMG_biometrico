05cd4cc12c1d94cf50e3ee0aa10f3685a4440b07
Dfrancoc256 <dfrancoc5@miumg.edu.gt>
2026-05-05 13:29:39 -0600

    cambios en el registro de cambios guardar y editar

---
+++ biometrico-umg/src/main/java/com/umg/biometrico/repository/RolRepository.java
@@ -0,0 +1,12 @@
+package com.umg.biometrico.repository;
+
+import com.umg.biometrico.model.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RolRepository extends JpaRepository<Rol, Long> {
+
+    Optional<Rol> findByNombre(String nombre);
+
+}
